cba90fc0f71d278933e6a4fe3d6c2cb0e5ab4bfc
Ernesto Fries Urioste <efries@inetpeople.net>
2019-11-09 19:42:44 +0800

    let Ecto know where to find the DB

--- lib/crypto_monkey/repo.ex
+++ lib/crypto_monkey/repo.ex
@@ -1,5 +1,10 @@
 defmodule CryptoMonkey.Repo do
   use Ecto.Repo,
     otp_app: :crypto_monkey,
-    adapter: Ecto.Adapters.Postgres
+    adapter: Ecto.Adapters.Postgres,
+    pool_size: System.get_env("POOL_SIZE")
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
