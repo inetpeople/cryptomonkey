2575723c1f054cf7be034ffc5b6b847497c5eea2
Ernesto Fries Urioste <efries@inetpeople.net>
2021-02-15 16:37:33 +0800

    retake work

--- lib/crypto_monkey_web/live/ticker_live/index.ex
+++ lib/crypto_monkey_web/live/ticker_live/index.ex
@@ -11,7 +11,8 @@ defmodule CryptoMonkeyWeb.TickerLive.Index do
       assign(socket,
         available_tickers: available_tickers(),
         query: "",
-        results: %{}
+        results: [],
+        loading: false
       )
 
     {:ok, socket}
@@ -19,21 +20,52 @@ defmodule CryptoMonkeyWeb.TickerLive.Index do
 
   @impl true
   def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+    result = suggest(query)
+    socket = assign(socket, results: result)
+
+    {:noreply, socket}
   end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+  def handle_event("search", %{"q" => ticker}, socket) do
+    send(self(), {:run_search, ticker})
 
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
+    socket =
+      assign(socket,
+        loading: true,
+        results: [],
+        ticker: ticker
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_info("run_search", %{"q" => query}, socket) do
+    case search(query) do
+      [] ->
+        socket =
+          socket
+          |> put_flash(:info, "No results matching \"#{query}\"")
+          |> assign(results: [], loading: false)
+
+        {:noreply, socket}
+
+      result ->
+        socket =
+          socket
+          |> clear_flash()
+          |> assign(results: result, loading: false)
+
+        {:noreply, socket}
     end
+
+    # %{^query => vsn} ->
+    #   {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+
+    # _ ->
+    #   {:noreply,
+    #    socket
+    #    |> put_flash(:error, "No dependencies found matching \"#{query}\"")
+    #    |> assign(results: %{}, query: query)}
   end
 
   @impl true
@@ -43,13 +75,19 @@ defmodule CryptoMonkeyWeb.TickerLive.Index do
   end
 
   defp search(query) do
-    for ticker <- available_tickers
-        String.starts_with?(ticker, query),
-        into: %{},
-        do: {app, vsn}
   end
 
   defp available_tickers do
     ["PI_ETHUSD", "PI_XBTUSD"]
+  end
+
+  def suggest(""), do: []
+
+  def suggest(prefix) do
+    Enum.filter(available_tickers(), &has_prefix?(&1, prefix))
+  end
+
+  defp has_prefix?(city, prefix) do
+    String.contains?(String.downcase(city), String.downcase(prefix))
   end
 end
